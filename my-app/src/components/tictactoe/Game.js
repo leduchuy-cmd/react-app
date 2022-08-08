ea3d7910ca1a732c691dd4066547ee371f00884d
HUY LE <huyle010998@gmail.com>
2022-08-09 00:04:36 +0700

    upload code

--- my-app/src/components/tictactoe/Game.js
+++ my-app/src/components/tictactoe/Game.js
@@ -8,42 +8,71 @@ const initialState = {
   xIsNext: true,
 };
 
+// immutable không thể chỉnh sửa
 const gameReducer = (state, action) => {
   switch (action.type) {
-    case "CLICK":
-      break;
+    case "CLICK": {
+      // console.log("it working");
+      const { board, xIsNext } = state;
+      const { index, winner } = action.payload;
+
+      if (winner || board[index]) return state;
+      const nextState = JSON.parse(JSON.stringify(state));
+
+      nextState.board[index] = xIsNext ? "X" : "O";
+      nextState.xIsNext = !xIsNext;
+      return nextState;
+    }
+    case "RESET": {
+      const nextState = JSON.parse(JSON.stringify(state));
+      nextState.board = Array(9).fill(null);
+      nextState.xIsNext = true;
+      return nextState;
+    }
 
     default:
       break;
   }
+  return state;
 };
 const Game = () => {
   // const
   const [state, dispatch] = useReducer(gameReducer, initialState);
 
-  const [state, setState] = useState({
-    board: Array(9).fill(null),
-    xIsNext: true,
-    name: "teo",
-  });
+  // const [state, setState] = useState({
+  //   board: Array(9).fill(null),
+  //   xIsNext: true,
+  //   name: "teo",
+  // });
   const winner = caculateWinner(state.board);
 
   const handleClick = (index) => {
-    const boardCopy = [...state.board];
-    if (winner || boardCopy[index]) return;
-    boardCopy[index] = state.xIsNext ? "X" : "O";
-    setState({
-      ...state,
-      board: boardCopy,
-      xIsNext: !state.xIsNext,
+    // const boardCopy = [...state.board];
+    // if (winner || boardCopy[index]) return;
+    dispatch({
+      type: "CLICK",
+      payload: {
+        index,
+        winner,
+      },
     });
+    // boardCopy[index] = state.xIsNext ? "X" : "O";
+    // setState({
+    //   ...state,
+    //   board: boardCopy,
+    //   xIsNext: !state.xIsNext,
+    // });
     // setBoard(boardCopy);
     // setXIsNext((xIsNext) => !xIsNext);
   };
 
   const handleResetGame = () => {
-    state.setBoard(Array(9).fill(null));
-    state.setXIsNext(true);
+    dispatch({
+      type: "RESET",
+      payload: {},
+      // state.setBoard(Array(9).fill(null));
+      // state.setXIsNext(true);
+    });
   };
   return (
     <div>
